7771ba0cf7331755154f9399c4351a9a6025801f
Lakori4 <santivalde04@gmail.com>
2025-07-10 13:44:01 +0200

    little fix

--- components/CharacterDetail.tsx
+++ components/CharacterDetail.tsx
@@ -12,7 +12,7 @@ const CharacterDetail:FunctionalComponent<{char: Character}> = (props) => {
         <div class="detail">
             <img src={props.char.image} alt={props.char.name}/>
 
-            <h2>{props.char.name} <Star id={props.char}/></h2>
+            <h2>{props.char.name} <Star id={props.char.id}/></h2>
             <p>Casa: {props.char.house}</p>
             {props.char.alive ? <p>Vivo</p>: <p>Muerto</p>}
             <a href="/" data-ancestor="true" aria-current="true">Volver</a>
